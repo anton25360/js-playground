175d86158d52a9f014006c2728d82c64983c5bca
Anton Webb <anton25360@gmail.com>
2019-10-02 09:52:09 +0100

    Added .gitignore. Output array on screen. Output person's age on click as a popup.

--- hello.js
+++ hello.js
@@ -1,59 +1,30 @@
-var hello ='Hello world';
-
+var hello ='Hello, world!';
 console.log(hello);
-
-function foo(name){
-    console.log(name)
-}
-
-foo('mike');
-
-function getName(x) {
-    x.name = 'john'
-    return x.name
-}
-
-var person = {
-    name: 'mike',
-    age : 27,
-    child: {
-        name: 'bob'
-    }
-}
-
-console.log(person.name) // mike
-
-let result = getName(person)
-
-console.log(result) // ? john
-console.log(person.name) // ? john
-console.log('')
-
 
 document.querySelector('h1').innerText = 'mike'
 document.querySelector('h1').style.background = '#ff0000'
-//alert('hello') //popup
 
+//array
 let people = [
-    { name : 'antonnnnn', age : 18 },
+    { name : 'anton', age : 18 },
     { name : 'mike', age : 27 },
     { name : 'god', age : 100 }
 ]
 
-//console.log(people[1])
-
+//outputs the array on the screen
 people.forEach(function(element) {
-
     console.log(element['name'])
-    document.querySelector('#parent').innerHTML += '<p data-age='+ element.age + '>' + element.name + '</p>'
+    document.querySelector('#parent').innerHTML += '<p class="title" data-age='+ element.age + '>' + element.name + '</p>' 
 });
 
+//outputs the person's age as a popup
+document.querySelectorAll('p').forEach(element => {
+    element.addEventListener('click',function() {
+        alert(element.getAttribute('data-age'))
+    })
+});
 
+//button popup
 document.querySelector('button').addEventListener('click', function() {
     alert('you clicked a button')
 })
-
-
-
-// [EXERCISE] - Create an array of objects containing a name and age.
-// Output each persons name in a <p> with their age as a data attribute.
